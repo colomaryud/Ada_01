17919653d168458cd7f4687abcfb2f13ca2af4cd
BlascoMayordomo <31961407+BlascoMayordomo@users.noreply.github.com>
2017-09-21 15:36:57 +0200

    Kirby-Bola y movimiento LR
    He metido el sprite de kirby bola y le he añadido un script de movimiento de izquierda y derecha con las teclas A y D.

---
+++ Proyecto_Ada_01/Assets/KirbyBola.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KirbyBola : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector2.right * Time.deltaTime * 4);
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Translate(Vector2.left * Time.deltaTime * 4);
+		}
+	}
+}
